f4d6c3517dfdb448b5851b63185a6d5cadf026f4
ImMdSuhailAkter <146857907+ImMdSuhailAkter@users.noreply.github.com>
2023-10-19 00:51:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var typed = new Typed(".text", {
+    strings: ["a front-end developer", "an artist", "a web developer","an engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+var typed = new Typed(".text2", {
+    strings: ["MD SUHAIL AKTER"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+var typed = new Typed(".text3", {
+    strings: ["H","e","l","l","o","Hello"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
